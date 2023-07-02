c96a55d6ac1b434d5f26f3b290d338241f44bd4e
FinJG <finlay.griffiths03@gmail.com>
2023-07-02 21:07:24 +0100

    added horizontal collision

--- player.py
+++ player.py
@@ -4,30 +4,37 @@ import math
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((30, 30))
-        self.image.fill((255, 255, 255))
+        self.image = pygame.Surface((32, 32))#pygame.image.load("sprites/player/player.png")
+
+        self.base_width = self.image.get_width() / 2
+        self.base_height = self.image.get_height() /2 
+        self.image.fill((255, 10, 20))
         self.rect = self.image.get_rect()
-        self.rect = pygame.Rect(self.rect.x, self.rect.y , 30, 30)
+        # self.rect = pygame.Rect(self.rect.x, self.rect.y , 30, 30)
         # self.rect.center = (100, 100)
     
     def resize(self, tile_size, render_scale_x, render_scale_y):
-        self.image = pygame.transform.scale(self.image, (tile_size * render_scale_x, tile_size * render_scale_y))
+        self.image = pygame.transform.scale(self.image, (self.base_width * render_scale_x, self.base_height * render_scale_y))
  
 
-    def update(self, dt, tile_size, render_scale_x, render_scale_y, world):
-        if self.rect.width > tile_size * render_scale_x:
-            self.rect.width = tile_size * render_scale_x - 2
-            self.rect.height = tile_size * render_scale_y - 2
-
+    def update(self, dt, tile_size, world):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
             if world[(self.rect.x // tile_size), (self.rect.y // tile_size)] == 0:
                 self.rect.x -= 200 * dt
+        
+        if world[math.ceil(self.rect.x // tile_size), math.ceil(self.rect.y // tile_size)] != 0:
+            self.rect.x = ((self.rect.x // tile_size) + 1) * tile_size
 
         if keys[pygame.K_d]:
             if world[(self.rect.x // tile_size) + 1, (self.rect.y // tile_size)] == 0:
                 self.rect.x += 200 * dt
+            
+
+        if world[math.ceil(self.rect.x // tile_size), math.ceil(self.rect.y // tile_size)] != 0:
+            self.rect.x = self.rect.x // tile_size * tile_size
+                
         if keys[pygame.K_w]:
             self.rect.y -= 600 * dt
 
