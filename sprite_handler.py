1aa969331497993fa4eddb75219318072689f5ee
FinJG <finlay.griffiths03@gmail.com>
2023-07-05 21:28:05 +0100

    improved animation handling

--- sprite_handler.py
+++ sprite_handler.py
@@ -3,21 +3,18 @@ import os
 
 dirt_1 = pygame.image.load("sprites/dirt_1.png")
 
-player_right = pygame.image.load("sprites/player/player_right.png") 
-player_left = pygame.image.load("sprites/player/player_left.png")
-
-
-
 class Animation():
     def __init__(self, animation_frames, duration) -> None:
-        self.animation = animation_frames
+        self.frames = animation_frames
         self.frame_count = len(animation_frames)
         self.duration = duration
 
-def load_animation(path, duration, animation_dict):
+animation_dict = {}
+def load_animation(path, duration):
     for i in os.listdir(path):
         if i.endswith(".png"):
             animation = [pygame.image.load(f"{path}/{i}") for i in os.listdir(path)]
+        
 
     if path.split("/")[-1] not in animation_dict:
         animation_dict[path.split("/")[-1]] = Animation(animation, duration)
@@ -25,8 +22,6 @@ def load_animation(path, duration, animation_dict):
     
     else:
         raise Exception("Animation already exists")
-
-
 
 grass_neighbors = [
     (False, False, False, False),
@@ -47,6 +42,6 @@ grass_neighbors = [
     (False, True, True, False),
 ]
 
-grass_images ={}
+grass_images = {}
 for i, v in enumerate(grass_neighbors):
     grass_images[v] = pygame.image.load(f"sprites/grass/grass_{i + 1}.png")
