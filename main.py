f768c7b152732c6ea37687eb1a5f0d28f0d03d23
FinJG <finlay.griffiths03@gmail.com>
2023-07-07 14:01:41 +0100

    added "main" function

--- main.py
+++ main.py
@@ -6,135 +6,138 @@ from player import Player
 from world import World
 import sprite_handler
 
-pygame.init()
+def main():
+    pygame.init()
 
-# Constants
-WIDTH, HEIGHT = SCREEN_SIZE = 960, 960
-FPS = 60
+    # Constants
+    WIDTH, HEIGHT = SCREEN_SIZE = 960, 960
+    FPS = 60
 
-# Load images
-sprite_handler.load_animation("sprites/player/walking_right", 0.2)
-sprite_handler.load_animation("sprites/player/walking_left", 0.2)
-sprite_handler.load_animation("sprites/player/idle", 1)
-sprite_handler.load_animation("sprites/player/breaking_right", 0.1)
+    # Load images
+    sprite_handler.load_animation("sprites/player/walking_right", 0.2)
+    sprite_handler.load_animation("sprites/player/walking_left", 0.2)
+    sprite_handler.load_animation("sprites/player/idle", 1)
+    sprite_handler.load_animation("sprites/player/breaking_right", 0.1)
 
-# Variables
-screen = pygame.display.set_mode(SCREEN_SIZE)
-display = pygame.Surface((512, 512))
-clock = pygame.time.Clock()
-dt = 0
-player = Player()
-world = World(screen)
-world.generate()
+    # Variables
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    display = pygame.Surface((512, 512))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player()
+    world = World(WIDTH, HEIGHT)
+    world.generate()
 
-def check_neighbours(x, y, nums):
-    check = [False, False, False, False]
-    for i, v in enumerate([[-1, 0], [0, -1], [1, 0], [0, 1]]):
-        try: # TEMPORARY FIX
-            if world.array[x + v[0], y + v[1]] in nums:
-                check[i] = True
-        except IndexError:
-            pass
-    return tuple(check)
-
-
-def change_tile(tile):
-    player.breaking = True
-    x, y = pygame.mouse.get_pos()
-    multiplier = WIDTH / display.get_width()
-
-    x = int((x / multiplier + world.scroll[0]) // tile_size)
-    y = int((y / multiplier + world.scroll[1]) // tile_size)
-    if (x, y) not in player.get_touching() or tile == 0:
-        try:
-            world.array[x, y] = tile
-        except IndexError:
-            pass
+    def check_neighbours(x, y, nums):
+        check = [False, False, False, False]
+        for i, v in enumerate([[-1, 0], [0, -1], [1, 0], [0, 1]]):
+            try: # TEMPORARY FIX
+                if world.array[x + v[0], y + v[1]] in nums:
+                    check[i] = True
+            except IndexError:
+                pass
+        return tuple(check)
 
 
-def draw_chunk(chunk, chunk_start_x=0, chunk_start_y=0):
-    for ix, x in enumerate(chunk):
-        for iy, y in enumerate(x):
-            iix = (ix + chunk_start_x) * tile_size - world.scroll[0]
-            iiy = (iy + chunk_start_y) * tile_size - world.scroll[1]
+    def change_tile(tile):
+        player.breaking = True
+        x, y = pygame.mouse.get_pos()
+        multiplier = WIDTH / display.get_width()
 
-            # draw dirt
-            if y == 1: 
-                display.blit(sprite_handler.dirt_1, (iix, iiy))
+        x = int((x / multiplier + world.scroll[0]) // world.tile_size)
+        y = int((y / multiplier + world.scroll[1]) // world.tile_size)
+        if (x, y) not in player.get_touching() or tile == 0:
+            try:
+                world.array[x, y] = tile
+            except IndexError:
+                pass
 
-            # draw grass
-            elif y == 2:
-                surrounding = check_neighbours(ix + chunk_start_x , iy + chunk_start_y, [1, 2])
 
-                # turn grass into dirt if grass is surrounded by tiles
-                if surrounding == (True, True, True, True): 
-                    chunk[ix, iy] = 1
+    def draw_chunk(chunk, collision_tiles, chunk_start_x=0, chunk_start_y=0):
+        for ix, x in enumerate(chunk):
+            for iy, y in enumerate(x):
+                iix = (ix + chunk_start_x) * world.tile_size - world.scroll[0]
+                iiy = (iy + chunk_start_y) * world.tile_size - world.scroll[1]
+
+                # draw dirt
+                if y == 1: 
                     display.blit(sprite_handler.dirt_1, (iix, iiy))
-                else:
-                    display.blit(sprite_handler.grass_images.get(surrounding, sprite_handler.grass_images[(False, False, False, False)]), (iix, iiy))
-            if y != 0:
-                collision_tiles.append(pygame.Rect((ix + chunk_start_x) * tile_size, iiy, tile_size, tile_size))
 
+                # draw grass
+                elif y == 2:
+                    surrounding = check_neighbours(ix + chunk_start_x , iy + chunk_start_y, [1, 2])
 
-# Game loop
-while True:
-    player.grid_x = player.rect.x // world.tile_size
-    player.grid_y = player.rect.y // world.tile_size
+                    # turn grass into dirt if grass is surrounded by tiles
+                    if surrounding == (True, True, True, True): 
+                        chunk[ix, iy] = 1
+                        display.blit(sprite_handler.dirt_1, (iix, iiy))
+                    else:
+                        display.blit(sprite_handler.grass_images.get(surrounding, sprite_handler.grass_images[(False, False, False, False)]), (iix, iiy))
+                if y != 0:
+                    collision_tiles.append(pygame.Rect((ix + chunk_start_x) * world.tile_size, iiy, world.tile_size, world.tile_size))
+        return collision_tiles
 
-    # i really need to get the y axis working
-    world.rendering_pos[0] += (player.rect.x - world.rendering_pos[0] - ((display.get_width() / 2) - player.rect.width / 2)) / 10
-    # world.rendering_pos[1] += (player.rect.y - world.rendering_pos[1])
+    # Game loop
+    while True:
+        player.grid_x = player.rect.x // world.tile_size
+        player.grid_y = player.rect.y // world.tile_size
 
-    world.scroll = world.rendering_pos.copy()
-    world.scroll = [int(i) for i in world.scroll]
+        # i really need to get the y axis working
+        world.rendering_pos[0] += (player.rect.x - world.rendering_pos[0] - ((display.get_width() / 2) - player.rect.width / 2)) / 10
+        # world.rendering_pos[1] += (player.rect.y - world.rendering_pos[1])
 
-    # draw sky
-    display.fill((20, 150, 230))
+        world.scroll = world.rendering_pos.copy()
+        world.scroll = [int(i) for i in world.scroll]
 
-    # Event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        # draw sky
+        display.fill((20, 150, 230))
 
-        # get key inputs
-        if event.type == pygame.KEYDOWN:
+        # Event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-            # reset world if r is pressed
-            if event.key == pygame.K_r:
-                world = World(screen)
-                player = Player()
-                world.generate()
-    
-    collision_tiles = []
-    tile_size = world.tile_size
+            # get key inputs
+            if event.type == pygame.KEYDOWN:
 
-    # draw tiles being rendered
-    for jx in range(0, 2):
-        for jy in range(0, 2):
-            thing = world.CHUNK_SIZE * (jx + (player.grid_x // world.CHUNK_SIZE))
-            thing2 = world.CHUNK_SIZE * (jy + (player.grid_y // world.CHUNK_SIZE))
-            if jx == 0:
-                draw_chunk(world.array[thing - world.CHUNK_SIZE:thing, thing2 - world.CHUNK_SIZE:thing2], thing - world.CHUNK_SIZE, 0)
-            draw_chunk(world.array[thing:world.CHUNK_SIZE + thing,0:world.CHUNK_SIZE + 0], thing, 0)
+                # reset world if r is pressed
+                if event.key == pygame.K_r:
+                    world = World(WIDTH, HEIGHT)
+                    player = Player()
+                    world.generate()
+        
+        collision_tiles = []
 
-    # break tiles
-    if pygame.mouse.get_pressed()[0]:
-        change_tile(0)
+        # draw tiles being rendered
+        for jx in range(0, 2):
+            for jy in range(0, 2):
+                thing = world.CHUNK_SIZE * (jx + (player.grid_x // world.CHUNK_SIZE))
+                thing2 = world.CHUNK_SIZE * (jy + (player.grid_y // world.CHUNK_SIZE))
+                if jx == 0:
+                    collision_tiles = draw_chunk(world.array[thing - world.CHUNK_SIZE:thing, thing2 - world.CHUNK_SIZE:thing2], collision_tiles, thing - world.CHUNK_SIZE, 0)
+                collision_tiles = draw_chunk(world.array[thing:world.CHUNK_SIZE + thing,0:world.CHUNK_SIZE + 0], collision_tiles, thing, 0)
 
-    # place tiles
-    if pygame.mouse.get_pressed()[2]:
-        change_tile(2)
+        # break tiles
+        if pygame.mouse.get_pressed()[0]:
+            change_tile(0)
 
-    # update player
-    player.update(collision_tiles, dt, world)
+        # place tiles
+        if pygame.mouse.get_pressed()[2]:
+            change_tile(2)
 
-    # # draw player
-    display.blit(player.image, (player.rect.x - world.scroll[0], player.rect.y - world.scroll[1]))
+        # update player
+        player.update(collision_tiles, dt, world)
 
-    # draw display to screen
-    screen.blit(pygame.transform.scale(display, SCREEN_SIZE), (0, 0))
+        # # draw player
+        display.blit(player.image, (player.rect.x - world.scroll[0], player.rect.y - world.scroll[1]))
 
-    # update screen
-    pygame.display.update()
-    dt = clock.tick(FPS) / 1000
+        # draw display to screen
+        screen.blit(pygame.transform.scale(display, SCREEN_SIZE), (0, 0))
+
+        # update screen
+        pygame.display.update()
+        dt = clock.tick(FPS) / 1000
+
+if __name__ == "__main__":
+    main()
--- world.py
+++ world.py
@@ -1,14 +1,14 @@
 import numpy
 
 class World():
-    def __init__(self, screen) -> None:
+    def __init__(self, WIDTH, HEIGHT) -> None:
         self.rendering_pos = [0, 0]
         self.rendering_width = 16
         self.rendering_height = 16
         self.CHUNK_SIZE = 16
         self.tile_size = 32
-        self.WORLD_WIDTH = (screen.get_width() // self.tile_size) * 10
-        self.WORLD_HEIGHT = (screen.get_height() // self.tile_size)
+        self.WORLD_WIDTH = WIDTH // self.tile_size * 10
+        self.WORLD_HEIGHT = HEIGHT // self.tile_size
         self.array = numpy.zeros((self.WORLD_WIDTH, self.WORLD_HEIGHT), dtype=int)
         self.scroll = [0, 0]
 
